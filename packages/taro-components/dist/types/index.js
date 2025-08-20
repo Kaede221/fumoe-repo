5b4c0f536bfc9c8ab7b4a48135eddf7962ec23fd
kaede221 <kaedeshimizu@qq.com>
2025-08-20 18:33:07 +0800

    init:删除不需要的东西

--- packages/taro-components/dist/types/index.js
+++
@@ -1,3 +0,0 @@
-export { default as MoeHeader } from './components/MoeHeader/index.tsx';
-export { default as MoePicker } from './components/MoePicker/index.tsx';
-export { default as MoeTag } from './components/MoeTag/index.tsx';
--- packages/taro-components/src/index.ts
+++
@@ -1,8 +0,0 @@
-export type { IMoeHeader } from "./components/MoeHeader/index";
-export { default as MoeHeader } from "./components/MoeHeader/index";
-
-export type { IMoePicker } from "./components/MoePicker/index";
-export { default as MoePicker } from "./components/MoePicker/index";
-
-export type { IMoeTag } from "./components/MoeTag/index";
-export { default as MoeTag } from "./components/MoeTag/index";
