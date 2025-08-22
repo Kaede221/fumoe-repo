fa92ebd1b78b6b3a59ffe144bed40020686f1db3
kaede221 <kaedeshimizu@qq.com>
2025-08-22 21:55:58 +0800

    feat:Cell 单元格添加左侧图标属性

--- apps/moe-lib/src/pages/index/index.tsx
+++ apps/moe-lib/src/pages/index/index.tsx
@@ -1,5 +1,7 @@
 import { View } from "@tarojs/components";
-import { MoeHeader } from "@fumoe/taro-components";
+import { MoeHeader, MoeCell } from "@fumoe/taro-components";
+
+import testIcon from "@/assets/testIcon.png";
 
 import "./index.scss";
 
@@ -7,6 +9,7 @@ const Index = () => {
   return (
     <View>
       <MoeHeader backgroundColor="#FFFFFF">MoeLib - 组件</MoeHeader>
+      <MoeCell title="你好" label="看起来不错" icon={testIcon} isLink></MoeCell>
     </View>
   );
 };
--- packages/components/src/MoeCell/index.tsx
+++ packages/components/src/MoeCell/index.tsx
@@ -20,6 +20,10 @@ export interface IMoeCell {
    */
   label?: React.ReactNode;
   /**
+   * 左侧图标部分图片链接
+   */
+  icon?: string;
+  /**
    * 是否显示右侧小箭头
    */
   isLink?: boolean;
@@ -37,6 +41,7 @@ export interface IMoeCell {
  * 通用的单元格列表组件
  * @param title 左侧标题部分
  * @param label 右侧信息部分
+ * @param icon 左侧图标部分图片链接
  * @param isLink 是否显示右侧小箭头
  * @param clickable 是否可点击, 是否有点击效果
  * @param onClick 点击的回调函数
@@ -45,6 +50,7 @@ export interface IMoeCell {
 const MoeCell: React.FC<IMoeCell> = ({
   title,
   label,
+  icon,
   isLink,
   clickable = true,
   onClick,
@@ -60,7 +66,13 @@ const MoeCell: React.FC<IMoeCell> = ({
         if (clickable && onClick) onClick();
       }}
     >
-      <View className="moe-cell-container-title">{title}</View>
+      <View className="moe-cell-container-left-part">
+        {icon && (
+          <Image svg className="moe-cell-container-left-part-icon" src={icon} />
+        )}
+        <View className="moe-cell-container-left-part-title">{title}</View>
+      </View>
+
       <View className="moe-cell-container-content">
         <Text className="moe-cell-container-content-cell-text">{label}</Text>
         {isLink && (
