e940cbaca08bfe43637bdf187340d89bf7b0171e
kaede221 <kaedeshimizu@qq.com>
2025-08-22 10:10:34 +0800

    fix:移除无用的文档内容

--- apps/test/src/pages/index/index.tsx
+++ apps/test/src/pages/index/index.tsx
@@ -1,16 +1,12 @@
 import { View } from "@tarojs/components";
 import { MoeHeader } from "@fumoe/taro-components";
-import UpdateHookTest from "@/components/UpdateHookTest";
 
 import "./index.scss";
 
 export default function Index() {
   return (
     <View className="index">
-      <MoeHeader back titleLeft>
-        测试场地
-      </MoeHeader>
-      <UpdateHookTest></UpdateHookTest>
+      <MoeHeader backgroundColor="#FFFFFF">测试场地</MoeHeader>
     </View>
   );
 }
