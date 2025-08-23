656b79a173fc8ebd56918bc2d874aaee7d853dff
kaede221 <kaedeshimizu@qq.com>
2025-08-23 22:56:35 +0800

    feat:优化显示逻辑

--- apps/moe-lib/src/components/normalComs/FloatButtonView/index.tsx
+++ apps/moe-lib/src/components/normalComs/FloatButtonView/index.tsx
@@ -8,17 +8,23 @@ import "./index.scss";
 
 const FloatButtonView: FC = () => {
   const [circleShape, { toggle: toggleCircleShape }] = useToggle(true);
+  const [visiable, { toggle: toggleVisiable }] = useToggle(false);
   const [colorIndex, { inc: incColorIndex }] = useCounter(0);
   const colorList = ["#008cff", "#6600ff", "#a100ff", "#ff9100"];
+  const [sizeIndex, { inc: incSizeIndex }] = useCounter(0);
+  const sizeList = ["small", "default", "large"] as const;
   return (
     <View className="float-button-view">
       {/* 提供控制原件 */}
       <MoeButton onClick={toggleCircleShape}>切换圆/方形</MoeButton>
       <MoeButton onClick={incColorIndex}>修改颜色</MoeButton>
+      <MoeButton onClick={incSizeIndex}>修改大小</MoeButton>
+      <MoeButton onClick={toggleVisiable}>切换是否显示</MoeButton>
       <MoeFloatButton
         shape={circleShape ? "circle" : "square"}
         backgroundColor={colorList[colorIndex % colorList.length]}
-        size="small"
+        size={sizeList[sizeIndex % sizeList.length]}
+        visible={visiable}
         icon={
           <Image
             src={refreshIcon}
--- apps/moe-lib/src/pages/index/index.tsx
+++ apps/moe-lib/src/pages/index/index.tsx
@@ -3,6 +3,8 @@ import { View } from "@tarojs/components";
 import { MoeHeader, MoeCell, IMoeCell } from "@fumoe/taro-components";
 import Title from "@/components/Title";
 
+import icDebug from "@/assets/ic-debug.svg";
+
 import "./index.scss";
 
 // 通用组件的渲染列表
@@ -33,6 +35,8 @@ const Index = () => {
       <MoeHeader
         backgroundColor="#FFFFFF"
         back
+        backIcon={icDebug}
+        iconSize={50}
         backHandler={async () => {
           await Taro.navigateTo({
             url: "/pages/debugPage/index",
@@ -42,15 +46,25 @@ const Index = () => {
         MoeLib - 组件
       </MoeHeader>
       <Title>通用组件</Title>
-      {renderList1.map((item, index) => (
-        <MoeCell
-          key={`renderlist1-${index}`}
-          title={item.title}
-          label={item.label}
-          isLink
-          onClick={item.onClick}
-        ></MoeCell>
-      ))}
+      {renderList1
+        .slice() // 创建数组副本，避免修改原数组
+        .sort((a, b) => {
+          // 提取首字母并转为小写进行比较（忽略大小写）
+          const firstLetterA = String(a.title).charAt(0).toLowerCase();
+          const firstLetterB = String(b.title).charAt(0).toLowerCase();
+          return firstLetterA.localeCompare(firstLetterB);
+        })
+        .map((item, index) => (
+          <MoeCell
+            key={`renderlist1-${index}`}
+            title={item.title}
+            label={item.label}
+            isLink
+            onClick={item.onClick}
+          ></MoeCell>
+        ))}
+
+      <Title>表单组件</Title>
     </View>
   );
 };
