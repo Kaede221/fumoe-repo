3ff660a0446d252feecee2776ff7d21cc8f9cf7c
kaede221 <kaedeshimizu@qq.com>
2025-08-24 20:20:24 +0800

    perf:优化渲染流程

--- apps/moe-lib/src/pages/index/index.tsx
+++ apps/moe-lib/src/pages/index/index.tsx
@@ -5,81 +5,58 @@ import Title from "@/components/Title";
 
 import "./index.scss";
 
-// 通用组件的渲染列表
 const renderList1: IMoeCell[] = [
   {
     title: "MoeButton",
     label: "按钮组件",
-    onClick: async () => {
-      await Taro.navigateTo({
-        url: "/pages/comsPage/index" + "?id=MoeButton",
-      });
-    },
   },
   {
     title: "MoeFloatButton",
     label: "悬浮按钮组件",
-    onClick: async () => {
-      await Taro.navigateTo({
-        url: "/pages/comsPage/index" + "?id=MoeFloatButton",
-      });
-    },
   },
   {
     title: "MoeCell",
     label: "单元格组件",
-    onClick: async () => {
-      await Taro.navigateTo({
-        url: "/pages/comsPage/index" + "?id=MoeCell",
-      });
-    },
   },
   {
     title: "MoeHeader",
     label: "通用的顶部组件",
-    onClick: async () => {
-      await Taro.navigateTo({
-        url: "/pages/comsPage/index" + "?id=MoeHeader",
-      });
-    },
   },
   {
     title: "MoeTag",
     label: "标签组件",
-    onClick: async () => {
-      await Taro.navigateTo({
-        url: "/pages/comsPage/index" + "?id=MoeTag",
-      });
-    },
   },
-]
-  .slice()
-  .sort((a, b) => {
-    // 提取首字母并转为小写进行比较（忽略大小写）
-    const firstLetterA = String(a.title).charAt(3).toLowerCase();
-    const firstLetterB = String(b.title).charAt(3).toLowerCase();
-    return firstLetterA.localeCompare(firstLetterB);
-  });
+];
 
 // 表单组件的渲染列表
 const renderList2: IMoeCell[] = [
   {
     title: "MoePicker",
     label: "弹出选择器",
-    onClick: async () => {
-      await Taro.navigateTo({
-        url: "/pages/comsPage/index" + "?id=MoePicker",
-      });
-    },
   },
-]
-  .slice()
-  .sort((a, b) => {
-    // 提取首字母并转为小写进行比较（忽略大小写）
-    const firstLetterA = String(a.title).charAt(3).toLowerCase();
-    const firstLetterB = String(b.title).charAt(3).toLowerCase();
-    return firstLetterA.localeCompare(firstLetterB);
-  });
+];
+
+// 最终渲染列表
+const finalList = [
+  {
+    title: "基础组件",
+    item: renderList1.slice().sort((a, b) => {
+      // 提取首字母并转为小写进行比较（忽略大小写）
+      const firstLetterA = String(a.title).charAt(3).toLowerCase();
+      const firstLetterB = String(b.title).charAt(3).toLowerCase();
+      return firstLetterA.localeCompare(firstLetterB);
+    }),
+  },
+  {
+    title: "表单组件",
+    item: renderList2.slice().sort((a, b) => {
+      // 提取首字母并转为小写进行比较（忽略大小写）
+      const firstLetterA = String(a.title).charAt(3).toLowerCase();
+      const firstLetterB = String(b.title).charAt(3).toLowerCase();
+      return firstLetterA.localeCompare(firstLetterB);
+    }),
+  },
+];
 
 const Index = () => {
   return (
@@ -87,26 +64,23 @@ const Index = () => {
       <MoeHeader backgroundColor="#FFFFFF" iconSize={50}>
         MoeLib - 组件
       </MoeHeader>
-      <Title>通用组件</Title>
-      {renderList1.map((item, index) => (
-        <MoeCell
-          key={`renderlist1-${index}`}
-          title={item.title}
-          label={item.label}
-          isLink
-          onClick={item.onClick}
-        ></MoeCell>
-      ))}
-
-      <Title>表单组件</Title>
-      {renderList2.map((item, index) => (
-        <MoeCell
-          key={`renderlist2-${index}`}
-          title={item.title}
-          label={item.label}
-          isLink
-          onClick={item.onClick}
-        ></MoeCell>
+      {finalList.map((listObj) => (
+        <>
+          <Title>{listObj.title}</Title>
+          {listObj.item.map((item, index) => (
+            <MoeCell
+              key={`renderlist1-${index}`}
+              title={item.title}
+              label={item.label}
+              isLink
+              onClick={async () => {
+                await Taro.navigateTo({
+                  url: "/pages/comsPage/index?id=" + item.title,
+                });
+              }}
+            ></MoeCell>
+          ))}
+        </>
       ))}
     </View>
   );
