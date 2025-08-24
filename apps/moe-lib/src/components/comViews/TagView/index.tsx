bf040dbf06c9950d9c8c0d37817d09996a8c0bde
kaede221 <kaedeshimizu@qq.com>
2025-08-24 19:40:57 +0800

    feat:完善Tag组件, 并完善对应文档

--- apps/moe-lib/src/components/comViews/TagView/index.tsx
+++ apps/moe-lib/src/components/comViews/TagView/index.tsx
@@ -2,25 +2,112 @@ import { FC } from "react";
 import { View } from "@tarojs/components";
 import Title from "@/components/Title";
 
-import { MoeTag } from "@fumoe/taro-components";
+import iconTest from "@/assets/ic-debug.svg";
+
+import { MoeTag, MoeCell } from "@fumoe/taro-components";
 
 const TagView: FC = () => {
   return (
     <View>
       <Title>自定义颜色字段</Title>
-      <View
-        style={{
-          width: "100%",
-          padding: "0 20rpx",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "20rpx",
-        }}
-      >
-        <MoeTag label="默认颜色" />
-        <MoeTag label="蓝色" mainColor="#FFFFFF" backgroundColor="#FFFFFF" />
-      </View>
+      <MoeCell
+        title="default"
+        label={<MoeTag label="标签" color="default" />}
+      ></MoeCell>
+      <MoeCell
+        title="primary"
+        label={<MoeTag label="标签签" color="primary" />}
+      ></MoeCell>
+      <MoeCell
+        title="info"
+        label={<MoeTag label="标签签签" color="info" />}
+      ></MoeCell>
+      <MoeCell
+        title="warning"
+        label={<MoeTag label="标签签签签" color="warning" />}
+      ></MoeCell>
+      <MoeCell
+        title="danger"
+        label={<MoeTag label="标签签签签签" color="danger" />}
+      ></MoeCell>
+      <MoeCell
+        title="success"
+        label={<MoeTag label="标签签签签签签" color="success" />}
+      ></MoeCell>
+      <Title>不同样式</Title>
+      <MoeCell
+        title="outlined"
+        label={<MoeTag label="镂空效果" color="default" variant="outlined" />}
+      ></MoeCell>
+      <MoeCell
+        title="outlined + color"
+        label={<MoeTag label="镂空效果" color="success" variant="outlined" />}
+      ></MoeCell>
+      <Title>不同大小</Title>
+      <MoeCell
+        title="默认"
+        label={<MoeTag label="标签" color="default" size="default" />}
+      ></MoeCell>
+      <MoeCell
+        title="中"
+        label={<MoeTag label="标签" color="default" size="medium" />}
+      ></MoeCell>
+      <MoeCell
+        title="大"
+        label={<MoeTag label="标签" color="default" size="large" />}
+      ></MoeCell>
+
+      <Title>不同形状</Title>
+      <MoeCell
+        title="square"
+        label={
+          <MoeTag label="标签" color="default" size="default" shape="square" />
+        }
+      ></MoeCell>
+      <MoeCell
+        title="rounded"
+        label={
+          <MoeTag label="标签" color="primary" size="large" shape="rounded" />
+        }
+      ></MoeCell>
+      <MoeCell
+        title="roundedLeft"
+        label={
+          <MoeTag
+            label="标签"
+            color="danger"
+            variant="outlined"
+            size="default"
+            shape="roundedLeft"
+          />
+        }
+      ></MoeCell>
+      <MoeCell
+        title="roundedRight"
+        label={
+          <MoeTag
+            label="标签"
+            color="success"
+            size="default"
+            shape="roundedRight"
+          />
+        }
+      ></MoeCell>
+      <Title>带图标</Title>
+      <MoeCell
+        title="default"
+        label={<MoeTag label="标签" color="default" icon={iconTest} />}
+      ></MoeCell>
+      <MoeCell
+        title="default"
+        label={
+          <MoeTag label="标签" size="large" color="success" icon={iconTest} />
+        }
+      ></MoeCell>
+      <MoeCell
+        title="default"
+        label={<MoeTag label="标签" color="info" icon={iconTest} />}
+      ></MoeCell>
     </View>
   );
 };
--- packages/components/src/MoeTag/index.tsx
+++ packages/components/src/MoeTag/index.tsx
@@ -1,64 +1,85 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import { Image, View } from "@tarojs/components";
+import classNames from "classnames";
 
 import "./index.scss";
 
 export interface IMoeTag {
-  /** tag文本 */
+  /**
+   * 文本
+   */
   label: string;
-  /** tag的左侧图标路径 */
+  /**
+   * 左侧图标路径
+   */
   icon?: string;
-  /** 缩放比例 */
-  scale?: number;
-  /** 标签主色调 (文本, 边框) */
-  mainColor?: string;
-  /** 标签背景色 (背景) */
-  backgroundColor?: string;
-  /** 点击事件 */
+  /**
+   * 颜色
+   */
+  color?: "default" | "primary" | "info" | "success" | "warning" | "danger";
+  /**
+   * 样式
+   */
+  variant?: "outlined" | "contained";
+  /**
+   * 大小
+   */
+  size?: "large" | "medium" | "default";
+  /**
+   * 形状
+   */
+  shape?: "rounded" | "roundedRight" | "roundedLeft" | "square";
+  /**
+   * 点击事件
+   */
   onClick?: () => void;
+  /**
+   * 自定义样式
+   */
+  style?: CSSProperties;
 }
 
 /**
  * 通用标签组件
- * @param label 标签文本
- * @param icon tag的左侧图标路径
- * @param scale 缩放比例
- * @param mainColor 标签主色调 (文本, 边框)
- * @param backgroundColor 标签背景色 (背景)
+ * @param label 文本
+ * @param icon 左侧图标路径
+ * @param color 颜色
+ * @param variant 样式
+ * @param size 大小
+ * @param shape 形状
  * @param onClick 点击事件
- * @author Kaede221
+ * @param style 自定义样式
+ * @author kaedeshimizu
  * @email kaedeshimizu@qq.com
  */
 const MoeTag: React.FC<IMoeTag> = ({
   label,
   icon,
-  scale = 1,
-  mainColor = "#6C4E09",
-  backgroundColor = "#FFFAF1",
+  color = "default",
+  variant = "contained",
+  size = "default",
+  shape = "square",
   onClick,
+  style,
 }) => {
   return (
     <View
-      className="common-shared-tag-lc"
-      style={{
-        borderColor: mainColor,
-        backgroundColor: backgroundColor,
-        zoom: scale,
-      }}
+      className={classNames(
+        "common-shared-tag-lc",
+        `color-set-${color}`,
+        `variant-set-${variant}`,
+        `variant-set-${variant}-${color}`,
+        `size-set-${size}`,
+        `shape-set-${shape}`,
+      )}
+      style={style}
       onClick={(e) => {
         e.stopPropagation();
         if (onClick) onClick();
       }}
     >
-      {icon && <Image className="img-icon" src={icon} />}
-      <View
-        className="label"
-        style={{
-          color: mainColor,
-        }}
-      >
-        {label}
-      </View>
+      {icon && <Image className={`img-icon-${size}`} src={icon} />}
+      <View className="label">{label}</View>
     </View>
   );
 };
