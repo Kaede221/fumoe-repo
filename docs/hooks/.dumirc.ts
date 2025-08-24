21ad0eeca113d5c5178f8a944dbd3721901158f5
kaede221 <kaedeshimizu@qq.com>
2025-08-24 10:08:10 +0800

    init:初始化hooks库的docs

---
+++ docs/hooks/.dumirc.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  themeConfig: {
+    name: 'Taro Hooks',
+    logo: '/icon.png',
+    nav: [{ title: '快速开始', link: '/start' }],
+    footer: 'Powered By KaedeShimizu',
+  },
+});
---
+++ docs/hooks/docs/index.md
@@ -0,0 +1,10 @@
+---
+title: Taro Hooks
+hero:
+  title: Taro Hooks
+  description: Taro Hooks 钩子库的文档
+  actions:
+    - text: 快速开始
+      link: /start
+---
+
